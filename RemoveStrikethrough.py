f8e180753cb6af9cbb41cbc8d9b049816104580b
Matheus Siqueira <m03.siqueira@gmail.com>
2025-06-18 09:29:52 -0300

    save 2

--- RemoveStrikethrough.py
+++ RemoveStrikethrough.py
@@ -1,4 +1,5 @@
 from docx import Document
+import re
 
 def removerTextoTachado(caminho_entrada, caminho_saida):
     doc = Document(caminho_entrada)
@@ -14,8 +15,24 @@ def removerTextoTachado(caminho_entrada, caminho_saida):
 
         for texto in novas_runs:
             paragrafo.add_run(texto)  # adiciona texto não tachado
-            
+
+    # Chama a função para alterar os números ordinais
+    alterarSimboloOrdinal(doc)
+
     doc.save(caminho_saida)
     print(f'Documento salvo sem texto tachado em: {caminho_saida}')
 
-removerTextoTachado('entrada.docx', 'saida_sem_tachado.docx')
+def alterarSimboloOrdinal(doc):
+    # Define o padrão para buscar números seguidos de "o", "a", "s", etc.
+    padrao_ordinais = r'(\d+)(o|a|s)(?=\s|$)'
+
+    for paragrafo in doc.paragraphs:
+        for run in paragrafo.runs:
+            if run.font.strike:
+                continue  # ignora texto tachado
+            # Substitui o padrão de número ordinal com o símbolo '°'
+            texto_corrigido = re.sub(padrao_ordinais, r'\1°', run.text)
+            run.text = texto_corrigido
+
+removerTextoTachado('C:\\Matheus Siqueira\\Python\\RemoveStrikethrough\\LEI DE REGISTROS PÚBLICOS (TESTE).docx', 
+                    'C:\\Matheus Siqueira\\Python\\RemoveStrikethrough\\saida_sem_tachado.docx')
